49b99ee6775e56a751c21d7e792e35748582ec73
Tahir Ahmad <tahirahmad0234@gmail.com>
2022-03-16 01:52:07 +0600

    minor change

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Fontawsome_pro_5.11.2
+
+At first open it Vscode. After Run the code with vscode live server. And Enjoy it.
--- js/main.js
+++ js/main.js
@@ -1,109 +1,114 @@
-$( function() {
-	'use strict'
-	
-	var myTable = $( '#myTable' );
+$(function () {
+  "use strict";
 
-	$.ajax( {
-		url: 'faicons.json',
+  var myTable = $("#myTable");
 
-		beforeSend: () => {
-			$('#loding_awit').show();
-		},
+  $.ajax({
+    url: "../faicons.json",
 
-		complete: () => {
-			$('#loding_awit').remove();
-		},
+    beforeSend: () => {
+      $("#loding_awit").show();
+    },
 
-		success: async function( res ) {
-			
-			var categories = [ 'fa', 'fad', 'fal', 'far', 'fas', 'fab' ];
+    complete: () => {
+      $("#loding_awit").remove();
+    },
 
-			await res.forEach( icon => {
+    success: async function (res) {
+      var categories = ["fa", "fad", "fal", "far", "fas", "fab"];
 
-				var trElem = $( '<tr/>' );
-				trElem.append( $( '<td/>' ).text( icon[0] ) );
+      await res.map((icon) => {
+        var trElem = $("<tr/>");
+        trElem.append($("<td/>").text(icon[0]));
 
-				categories.forEach( category => {
+        categories.forEach((category) => {
+          var tdElem = $("<td/>", { class: "ico" });
 
-					var tdElem = $( '<td/>', { class: 'ico' } );
+          if (
+            (category == "fab" && icon[1] == 1) ||
+            (category != "fab" && icon[1] != 1)
+          ) {
+            var btnElem = $("<button/>", { class: "copy" }).html(
+              '<i class="fa fa-copy"></i>'
+            );
+            var data = '<i class="' + category + " " + icon[0] + '"></i>';
 
-					if ( ( category == 'fab' && icon[1] == 1 ) || ( category != 'fab' && icon[1] != 1 ) ) {
+            btnElem.on("click", function () {
+              var $temp = $("<input>");
+              $("body").append($temp);
+              $temp.val(data).select();
+              document.execCommand("copy");
+              $temp.remove();
+              $(".coppied").addClass("coppied_active");
+              $(".coppied").text("Coppied! Paste me ");
+              setTimeout(function () {
+                $(".coppied").removeClass("coppied_active");
+              }, 1200);
+            });
 
-						var btnElem = $( '<button/>', { class: 'copy' } ).html( '<i class="fa fa-copy"></i>' );
-						var data = '<i class="' + category + ' ' + icon[0] + '"></i>';
+            tdElem.append(data).append($("<div/>").append(btnElem));
+          }
 
-						btnElem.on( 'click', function() {
-							var $temp = $( "<input>" );
-							$( 'body' ).append( $temp );
-							$temp.val( data ).select();
-							document.execCommand("copy");
-							$temp.remove();
-							$(".coppied").addClass("coppied_active");
-							$(".coppied").text( "Coppied! Paste me ")
-							setTimeout(function(){
-								$(".coppied").removeClass("coppied_active");
-							}, 1200)
-						} );
+          trElem.append(tdElem);
+        });
 
-						tdElem.append( data ).append( $( '<div/>' ).append( btnElem ) );
-					}
+        myTable.append(trElem);
+      });
 
-					trElem.append( tdElem );
-				});
+      myTable.ready(function () {
+        $(window).scrollTop(0);
 
-				myTable.append( trElem );
+        console.log("%c Window Loaded!", "color: red; font-size:35px;");
+        console.log(
+          "%c Script Writter : Rakibul Islam",
+          "color: #0f84f1; font-size:14px; padding: 12px"
+        );
+        console.log(
+          "%c website Designer: Tahir Ahmad",
+          "color: #0f84f1; font-size:14px; padding: 12px"
+        );
+        console.log(
+          "%c Develped by Lazy Developers Community (LDC)",
+          "color: #0f84f1; font-size:14px; padding: 12px"
+        );
 
-			} );
-			
-			myTable.ready( function() {
-				$(window).scrollTop(0);
+        $("#search").on("click", function () {
+          $("#myInput").toggle(500, "swing");
+          $("#myInput").focus();
+          $("#search i").toggleClass("fa-search");
+          $("#search i").toggleClass("fa-arrow-left");
+          $("#search").toggleClass("s_b_r");
+        });
 
-				console.log("%c Window Loaded!", "color: red; font-size:35px;");
-				console.log("%c Script Writter : Rakibul Islam",  "color: #0f84f1; font-size:14px; padding: 12px");
-				console.log("%c website Designer: Tahir Ahmad",  "color: #0f84f1; font-size:14px; padding: 12px");
-				console.log("%c Develped by Lazy Developers Community (LDC)",  "color: #0f84f1; font-size:14px; padding: 12px");
+        $(window).on("scroll", function () {
+          var scroll = $(window).scrollTop();
 
-				$("#search").on("click",function(){
-					$("#myInput").toggle ( 500,"swing" );
-					$("#myInput").focus();
-					$("#search i").toggleClass("fa-search");
-					$("#search i").toggleClass("fa-arrow-left");
-					$("#search").toggleClass("s_b_r");
-			
-				});
+          if (scroll > 400) {
+            $("#top").addClass("top");
+          } else {
+            $("#top").removeClass("top");
+          }
 
-				$(window).on('scroll', function(){
-					var scroll = $(window).scrollTop();
-			
-					if (scroll > 400){
-						$('#top').addClass("top");
-					}else{
-						$('#top').removeClass("top")
-					}
-			
-					if (scroll > 15){
-						$('#thead tr').addClass("stickey");
-					}else{
-						$('#thead tr').removeClass("stickey")
-					}
-				});
+          if (scroll > 15) {
+            $("#thead tr").addClass("stickey");
+          } else {
+            $("#thead tr").removeClass("stickey");
+          }
+        });
 
-				$('#top').on('click', function(){
-					$(window).scrollTop(0)
-				});
+        $("#top").on("click", function () {
+          $(window).scrollTop(0);
+        });
+      });
+    },
+  });
 
-				
-			} )
-		},
-	
-	})
-
-	$(document).ready(() => {
-		$("#myInput").on("keyup", function() {
-			var value = $(this).val().toLowerCase();
-			$("#myTable tr").filter(function() {
-				$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-			});
-		});
-	})
-})
+  $(document).ready(() => {
+    $("#myInput").on("keyup", function () {
+      var value = $(this).val().toLowerCase();
+      $("#myTable tr").filter(function () {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+      });
+    });
+  });
+});
